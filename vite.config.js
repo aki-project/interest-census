aec977aa3f9dcbae669daef2ec764bb651277e25
aki-project <iwayuranee@gmail.com>
2025-10-27 12:42:55 -0700

    Add rollup-plugin-peer-deps-external and vite-plugin-singlefile.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    peerDepsExternal(),
+    react(), 
+    viteSingleFile(),
+  ],
+  esbuild: {
+    minifyIdentifiers: false,
+    minifyWhitespace: false,
+  },
+  minify: 'esbuild',
+  build: {
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime', 'react-dom', 'react-dom/client'], // Mark React and ReactDOM as external
+      output: {
+        paths: {
+          react: 'https://unpkg.com/react@^18/umd/react.development.js',
+          'react-dom': 'https://unpkg.com/react-dom@^18/umd/react-dom.development.js'
+        }
+      }
+    },
+  },
 })
